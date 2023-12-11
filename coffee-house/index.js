14768cb2eb1c7c894b94a749ca754a62d80d34df
Maslovars <maslovars@gmail.com>
2023-12-11 05:06:45 +0300

    feat: add index.js with console.log

---
+++ coffee-house/index.js
@@ -0,0 +1,27 @@
+console.log(`
+Score 100/100
+1. Check markup with https://validator.w3.org/: +12
+2. Favicon is added: +4
+3. <header> block on each page: +6
+4. Enjoy block on home page +6
+5. Favorites Coffee block on home page +6
+6. About block on home page +6
+7. Mobile App block on home page +6
+8. Menu block on menu page +6
+9. <footer> block on each page +6
+10. For positioning images used Flexbox or Grid Layout +4
+11. When scaling the browser page (<100%) or increasing the page width (>1440px), the layout of both pages is centered rather than shifted to the side and not stretched across the entire width +4
+12. The background color Body stretches across the entire width of the page +2
+13. Navigation elements (except Contacts) lead to corresponding blocks on home page (anchor links) +4
+14. Contacts in navigation panel links to the <footer> block on its own page (anchor link) +2
+15. Smooth scrolling with anchor links +2
+16. When clicking on the Menu buttons in header and Enjoy block on home page, it navigates to the menu page +2
+17. The Menu button in header on menu page is non-interactive +2
+18. When clicking on the Logo in header, it navigates to the home page +2
+19. The active Coffee button in Menu block of Menu page is non-interactive +2
+20. Each Coffee-card in the Menu section of the Menu page is interactive when hovering over any area of the card +4
+21. In the <footer> block, clicking on the link with phone number (all area including icon) should initiate a phone call +2
+22. In the <footer> block, clicking on the link with the address (all area including icon) should open a new browser tab with Google Maps displaying any location of your choice +2
+23. Interactivity +4
+24. Mandatory requirement for interactivity: smooth change in the appearance of an element on hover and click, without affecting adjacent elements +4
+`);
