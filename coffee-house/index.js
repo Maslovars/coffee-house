7d5b582d735a5a5f28c66dbb0aa1b847f12fbfcf
Maslovars <maslovars@gmail.com>
2023-12-25 04:03:05 +0300

    feat: disble mobile hover and add index.js

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,27 +1,13 @@
 console.log(`
-Score 100/100
-1. Check markup with https://validator.w3.org/: +12
-2. Favicon is added: +4
-3. <header> block on each page: +6
-4. Enjoy block on home page +6
-5. Favorites Coffee block on home page +6
-6. About block on home page +6
-7. Mobile App block on home page +6
-8. Menu block on menu page +6
-9. <footer> block on each page +6
-10. For positioning images used Flexbox or Grid Layout +4
-11. When scaling the browser page (<100%) or increasing the page width (>1440px), the layout of both pages is centered rather than shifted to the side and not stretched across the entire width +4
-12. The background color Body stretches across the entire width of the page +2
-13. Navigation elements (except Contacts) lead to corresponding blocks on home page (anchor links) +4
-14. Contacts in navigation panel links to the <footer> block on its own page (anchor link) +2
-15. Smooth scrolling with anchor links +2
-16. When clicking on the Menu buttons in header and Enjoy block on home page, it navigates to the menu page +2
-17. The Menu button in header on menu page is non-interactive +2
-18. When clicking on the Logo in header, it navigates to the home page +2
-19. The active Coffee button in Menu block of Menu page is non-interactive +2
-20. Each Coffee-card in the Menu section of the Menu page is interactive when hovering over any area of the card +4
-21. In the <footer> block, clicking on the link with phone number (all area including icon) should initiate a phone call +2
-22. In the <footer> block, clicking on the link with the address (all area including icon) should open a new browser tab with Google Maps displaying any location of your choice +2
-23. Interactivity +4
-24. Mandatory requirement for interactivity: smooth change in the appearance of an element on hover and click, without affecting adjacent elements +4
+Score 90/90
+1. The layout of the pages matches the design at a screen width of 1440px: +14
+2. The layout of the pages matches the design at a screen width of 768px: +14
+3. The layout of the pages matches the design at a screen width of 380px: +14
+4. There is no horizontal scroll bar at all screen width up to 380px inclusive. 
+All page content remains as per the design: it is not cropped, removed, or shifted to the side: +20
+5. During smooth resizing of the screen from 1440px to 380px, the layout takes up the full width of the window (including the margins specified in the layout), 
+elements change their sizes and positions (but without full scaling), elements do not overlap, and images maintain their correct proportions: +8
+6. At screen widths of 768px and below on both pages, the menu and navigation buttons in the header are hidden, and a burger menu icon appears: +4
+7. Hover effects are enable on desktop devices (Desktop device type in DevTools) and disabled for mobile devices on both pages (Mobile device type in DevTools): +4
+8. The layout of both pages is valid: to check the validity of the layout, use the service https://validator.w3.org/ : +12
 `);
