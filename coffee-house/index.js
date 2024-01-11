89f4e78b857e74e091edde18aeb0ba355b211023
Maslovars <maslovars@gmail.com>
2024-01-11 17:44:17 +0300

    feat: add video background to hero section

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,13 +1,1 @@
-console.log(`
-Score 90/90
-1. The layout of the pages matches the design at a screen width of 1440px: +14
-2. The layout of the pages matches the design at a screen width of 768px: +14
-3. The layout of the pages matches the design at a screen width of 380px: +14
-4. There is no horizontal scroll bar at all screen width up to 380px inclusive. 
-All page content remains as per the design: it is not cropped, removed, or shifted to the side: +20
-5. During smooth resizing of the screen from 1440px to 380px, the layout takes up the full width of the window (including the margins specified in the layout), 
-elements change their sizes and positions (but without full scaling), elements do not overlap, and images maintain their correct proportions: +8
-6. At screen widths of 768px and below on both pages, the menu and navigation buttons in the header are hidden, and a burger menu icon appears: +4
-7. Hover effects are enable on desktop devices (Desktop device type in DevTools) and disabled for mobile devices on both pages (Mobile device type in DevTools): +4
-8. The layout of both pages is valid: to check the validity of the layout, use the service https://validator.w3.org/ : +12
-`);
+console.log('123');
