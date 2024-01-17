5cfc80abbe792a325dbc82ed9ec4796edc635770
Maslovars <maslovars@gmail.com>
2024-01-17 19:13:56 +0300

    feat: add modal styles

--- coffee-house/menu/menu.js
+++ coffee-house/menu/menu.js
@@ -7,29 +7,29 @@ const modal = document.querySelector('.modal');
 let category = 'coffee';
 
 function setCategory() {
-    for (button of menuButtons) {
-        button.classList.remove('menu__btn_active', 'disabled');
-    }
-    category = this.value;
-    this.classList.add('menu__btn_active', 'disabled');
-    renderMenu(category);
+  for (button of menuButtons) {
+    button.classList.remove('menu__btn_active', 'disabled');
+  }
+  category = this.value;
+  this.classList.add('menu__btn_active', 'disabled');
+  renderMenu(category);
 }
 
 const loadMore = () => {
-    loadMoreButton.classList.add('hidden');
-    for (item of items) {
-        item.classList.add('displayed');
-    }
+  loadMoreButton.classList.add('hidden');
+  for (item of items) {
+    item.classList.add('displayed');
+  }
 }
 
 const renderMenu = (category) => {
-    menuPreview.innerHTML = '';
-    let i = 1;
-    fetch('./products.json').then((res) => res.json()).then((data) => {
-        data.forEach((item) => {
-            if (item.category === category) {
-                let itemHTML = `
-                    <div class="menu__preview-item">
+  menuPreview.innerHTML = '';
+  let i = 1;
+  fetch('./products.json').then((res) => res.json()).then((data) => {
+    data.forEach((item) => {
+      if (item.category === category) {
+        let itemHTML = `
+                  <div class="menu__preview-item">
                     <div class="menu__preview-img">
                       <img src="../assets/${category}/${category}-${i}.jpg" alt="${item.name}">
                     </div>
@@ -42,26 +42,26 @@ const renderMenu = (category) => {
                     </div>
                   </div>
                 `;
-                menuPreview.innerHTML += itemHTML;
-                i++;
-                if (loadMoreButton.classList.contains('hidden')) {
-                    loadMoreButton.classList.remove('hidden');
-                }
-                if (category === 'tea') {
-                    loadMoreButton.classList.add('hidden');
-                }
-            }
-        })
+        menuPreview.innerHTML += itemHTML;
+        i++;
+        if (loadMoreButton.classList.contains('hidden')) {
+          loadMoreButton.classList.remove('hidden');
+        }
+        if (category === 'tea') {
+          loadMoreButton.classList.add('hidden');
+        }
+      }
     })
+  })
 }
 
 function showModal(name) {
-    modal.innerHTML = '';
-    let i = 1;
-    fetch('./products.json').then((res) => res.json()).then((data) => {
-        data.forEach((card) => {
-            if (card.name === name) {
-                let cardHTML = `
+  modal.innerHTML = '';
+  let i = 1;
+  fetch('./products.json').then((res) => res.json()).then((data) => {
+    data.forEach((card) => {
+      if (card.name === name) {
+        let cardHTML = `
               <div class="modal__overlay">
               <div class="modal__container">
                 <div class="modal__img">
@@ -73,80 +73,80 @@ function showModal(name) {
                     <p class="modal__description">${card.description}</p>
                   </div>
                   <div>
-                    <div class="modal-coffee-size__title">Size</div>
-                    <div class="modal-coffee-size__block">
-                      <label class="modal-radio__label">
+                    <div class="modal__subtitle">Size</div>
+                    <div class="modal__subblock">
+                      <label class="modal__label">
                         <input type="radio" name="size" value="s" checked>
-                        <span class="modal-radio__btn">
-                          <span class="modal-radio__icon">S</span>
-                          <span class="modal-radio__size"></span>
+                        <span class="modal__btn">
+                          <span class="modal-btn__icon">S</span>
+                          <span class="modal-btn__description"></span>
                         </span>
                       </label>
-                      <label class="modal-radio__label">
+                      <label class="modal__label">
                         <input type="radio" name="size" value="m">
-                        <span class="modal-radio__btn">
-                          <span class="modal-radio__icon">M</span>
-                          <span class="modal-radio__size"></span>
+                        <span class="modal__btn">
+                          <span class="modal-btn__icon">M</span>
+                          <span class="modal-btn__description"></span>
                         </span>
                       </label>
-                      <label class="modal-radio__label">
+                      <label class="modal__label">
                         <input type="radio" name="size" value="l">
-                        <span class="modal-radio__btn">
-                          <span class="modal-radio__icon">L</span>
-                          <span class="modal-radio__size"></span>
+                        <span class="modal__btn">
+                          <span class="modal-btn__icon">L</span>
+                          <span class="modal-btn__description"></span>
                         </span>
                       </label>
                     </div>
                   </div>
                   <div>
-                    <div class="modal-option__title">Additives</div>
-                    <div class="modal-option__block">
-                      <label class="modal-option__label">
+                    <div class="modal__subtitle">Additives</div>
+                    <div class="modal__subblock">
+                      <label class="modal__label">
                         <input type="checkbox" value="1">
-                        <span class="modal-option__btn">
-                          <span class="modal-option__icon">1</span>
-                          <span class="modal-option__description"></span>
+                        <span class="modal__btn">
+                          <span class="modal-btn__icon">1</span>
+                          <span class="modal-btn__description"></span>
                         </span>
                       </label>
-                      <label class="modal-option__label">
+                      <label class="modal__label">
                         <input type="checkbox" value="2">
-                        <span class="modal-option__btn">
-                          <span class="modal-option__icon">2</span>
-                          <span class="modal-option__description"></span>
+                        <span class="modal__btn">
+                          <span class="modal-btn__icon">2</span>
+                          <span class="modal-btn__description"></span>
                         </span>
                       </label>
-                      <label class="modal-option__label">
+                      <label class="modal__label">
                         <input type="checkbox" value="3">
-                        <span class="modal-option__btn">
-                          <span class="modal-option__icon">3</span>
-                          <span class="modal-option__description"></span>
+                        <span class="modal__btn">
+                          <span class="modal-btn__icon">3</span>
+                          <span class="modal-btn__description"></span>
                         </span>
                       </label>
                     </div>
                   </div>
-                  <div class="total">
-                    <h3 class="total__title">Total:</h3>
-                    <h3 class="total__price"></h3>
+                  <div class="modal-total">
+                    <h3 class="modal-total__title">Total:</h3>
+                    <h3 class="modal-total__price"></h3>
                   </div>
                   <div class="modal-info">
                     <img src="../assets/info-empty.svg" alt="info icon">
                     <p class="modal-info__description">The cost is not final. Download our mobile app to see the final price and place your order. 
                     Earn loyalty points and enjoy your favorite coffee with up to 20% discount.</p>
                   </div>
-                  <div class="modal__close-btn">Close</div>
+                  <button class="modal__close-btn">Close</button>
                 </div>            
               </div>
             </div>
                 `;
-                modal.innerHTML += cardHTML;
-                i++;
-            }
-        })
+        modal.innerHTML += cardHTML;
+        i++;
+      }
     })
+  })
 }
 
 for (button of menuButtons) {
-    button.addEventListener('click', setCategory);
+  button.addEventListener('click', setCategory);
 }
 
 loadMoreButton.addEventListener('click', loadMore);
